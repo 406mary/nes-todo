13df85d4fbb37a7089d21a0d8332a0d07afc9ada
406mary <maryahoagland@gmail.com>
2021-08-31 10:04:35 -0500

    heroku deploy changes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ app.use(express.urlencoded({ extended: true }));
 const taskRouter = require('./routes/task_router');
 app.use('/tasks', taskRouter);
 
-const PORT = 5000;
+const PORT = process.envPORT || 5000;
 
 app.use(express.static('server/public'));
 
